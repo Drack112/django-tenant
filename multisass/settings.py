7a5f808d635674667bcc30b8ee11c84b5bbebece
João Vitor <61944386+Drack112@users.noreply.github.com>
2025-04-17 09:27:49 -0300

    feat: add debug toolbar and corsheaders to setup django

--- multisass/settings.py
+++ multisass/settings.py
@@ -61,7 +61,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "data_browser"
+    # Third party apps
+    "data_browser",
+    "corsheaders",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -72,6 +75,30 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.profiling.ProfilingPanel',
+    'debug_toolbar.panels.history.HistoryPanel',
+    'template_timings_panel.panels.TemplateTimings.TemplateTimings',
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+    "localhost",
+    "0.0.0.0",
+    "host.docker.internal",
+    "172.104.60.217"
 ]
 
 ROOT_URLCONF = 'multisass.urls'
