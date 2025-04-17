60b8b59e2e8642ed7d0806fa01bc67ddfe2e2404
João Vitor <61944386+Drack112@users.noreply.github.com>
2025-04-17 10:13:26 -0300

    feat: add .gitignore and update project dependencies; refactor settings and manage files for consistency

--- multisass/asgi.py
+++ multisass/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'multisass.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "multisass.settings")
 
 application = get_asgi_application()
--- multisass/settings.py
+++ multisass/settings.py
@@ -16,24 +16,31 @@ CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = config('SECRET_KEY', default='54g6s%qjfnhbpw0zeoei=$!her*y(p%!&84rs$4l85io')
-SECRET_KEY = config('SECRET_KEY', default=os.environ.get(
-    "DJANGO_SECRET_KEY", "54g6s%qjfnhbpw0zeoei=$!her*y(p%!&84rs$4l85io"))
+SECRET_KEY = config(
+    "SECRET_KEY",
+    default=os.environ.get(
+        "DJANGO_SECRET_KEY", "54g6s%qjfnhbpw0zeoei=$!her*y(p%!&84rs$4l85io"
+    ),
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config("DEBUG", default=True, cast=bool)
 
 if DEBUG:
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + \
-        ["10.0.2.2", "host.docker.internal", "47.128.216.140"]
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + [
+        "10.0.2.2",
+        "host.docker.internal",
+        "47.128.216.140",
+    ]
 
 # ALLOWED_HOSTS = [os.getenv("ALLOWED_PORTS")]
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 # Cors Settings
-BACKEND_DOMAIN = 'http://172.104.60.217:8585/'
-PAYMENT_SUCCESS_URL = 'http://172.104.60.217:8585/api/v1/products/success/'
-PAYMENT_CANCEL_URL = 'http://172.104.60.217:8585/api/v1/products/cancel/'
+BACKEND_DOMAIN = "http://172.104.60.217:8585/"
+PAYMENT_SUCCESS_URL = "http://172.104.60.217:8585/api/v1/products/success/"
+PAYMENT_CANCEL_URL = "http://172.104.60.217:8585/api/v1/products/cancel/"
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOWED_ORIGINS = [
@@ -42,55 +49,58 @@ CORS_ALLOWED_ORIGINS = [
     "https://792jz173sj.execute-api.us-east-1.amazonaws.com",
     "https://socialcloudsync.com",
     "http://52.90.4.135",
-    "http://52.90.4.135:8585"
+    "http://52.90.4.135:8585",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
 
 # Django data browser
 # References : https://pypi.org/project/django-data-browser/
-DATA_BROWSER_FE_DSN = "https://af64f22b81994a0e93b82a32add8cb2b@o390136.ingest.sentry.io/5231151"
+DATA_BROWSER_FE_DSN = (
+    "https://af64f22b81994a0e93b82a32add8cb2b@o390136.ingest.sentry.io/5231151"
+)
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # Third party apps
     "data_browser",
     "corsheaders",
     "debug_toolbar",
+    "template_timings_panel",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 DEBUG_TOOLBAR_PANELS = [
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.settings.SettingsPanel',
-    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.cache.CachePanel',
-    'debug_toolbar.panels.profiling.ProfilingPanel',
-    'debug_toolbar.panels.history.HistoryPanel',
-    'template_timings_panel.panels.TemplateTimings.TemplateTimings',
+    "debug_toolbar.panels.versions.VersionsPanel",
+    "debug_toolbar.panels.settings.SettingsPanel",
+    "debug_toolbar.panels.staticfiles.StaticFilesPanel",
+    "debug_toolbar.panels.timer.TimerPanel",
+    "debug_toolbar.panels.headers.HeadersPanel",
+    "debug_toolbar.panels.request.RequestPanel",
+    "debug_toolbar.panels.sql.SQLPanel",
+    "debug_toolbar.panels.cache.CachePanel",
+    "debug_toolbar.panels.profiling.ProfilingPanel",
+    "debug_toolbar.panels.history.HistoryPanel",
+    "template_timings_panel.panels.TemplateTimings.TemplateTimings",
 ]
 
 INTERNAL_IPS = [
@@ -98,42 +108,42 @@ INTERNAL_IPS = [
     "localhost",
     "0.0.0.0",
     "host.docker.internal",
-    "172.104.60.217"
+    "172.104.60.217",
 ]
 
-ROOT_URLCONF = 'multisass.urls'
+ROOT_URLCONF = "multisass.urls"
 TEMPLATE_DIR = os.path.join(CORE_DIR, "app/templates")
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [TEMPLATE_DIR],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'multisass.wsgi.application'
+WSGI_APPLICATION = "multisass.wsgi.application"
 
 
 # Database postgres Docker
 # Docker host : host.docker.internal  or database service name: prodxcloud-django-postgresdb
 # docker inspect prodxcloud-django-postgresdb | grep "IPAddress"
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("POSTGRES_NAME", "DB4"),
-        'USER': os.environ.get("POSTGRES_USER", "postgres"),
-        'PASSWORD': os.environ.get("POSTGRES_PASSWORD", "postgres"),
-        'HOST': os.environ.get("POSTGRES_HOST", "prodxcloud-django-postgresdb"),
-        'PORT': int(os.environ.get("POSTGRES_PORT", "5432")),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_NAME", "DB2"),
+        "USER": os.environ.get("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": int(os.environ.get("POSTGRES_PORT", "5432")),
     }
 }
 
@@ -160,16 +170,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -177,8 +187,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
@@ -186,7 +196,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_ROOT = os.path.join(CORE_DIR, "staticfiles")
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
@@ -198,13 +208,11 @@ if not os.path.exists(MEDIA_ROOT) or not os.path.exists(IMAGES_DIR):
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_PROFILE_MODULE = "auth.User"
-AUTH_USER_MODEL = 'auth.User'
+AUTH_USER_MODEL = "auth.User"
 
-STATICFILES_DIRS = (
-    os.path.join(CORE_DIR, "apps/static")
-)
+STATICFILES_DIRS = (os.path.join(CORE_DIR, "apps/static"),)
 
 # Caching parameters
 # MEMUSAGE_ENABLED = True
@@ -228,12 +236,13 @@ CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
 CELERY_RESULT_BACKEND = os.environ.get(
     "CELERY_RESULT_BACKEND", "redis://redis:6379/0")
 CELERY_BROKER_TRANSPORT_URL = os.environ.get(
-    "CELERY_RESULT_BACKEND", "redis://redis:6379/0")
+    "CELERY_RESULT_BACKEND", "redis://redis:6379/0"
+)
 # CELERY_RESULT_BACKEND= os.environ.get("CELERY_RESULT_BACKEND", "db+postgresql://postgres:postgres@172.104.60.217/DB2")
 BROKER_URL = os.environ.get("BROKER_URL", "redis://redis:6379/1")
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
+CELERY_ACCEPT_CONTENT = ["application/json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
 CELERY_TIMEZONE = "Asia/Singapore"
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
@@ -251,7 +260,7 @@ BROKER_CHANNEL_ERROR_RETRY = True
 BROKER_TRANSPORT = "kombu.transport.django"
 
 # Parameters for SMTP EMAIL EmailBackend
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", True)
 EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
 EMAIL_HOST_USER = os.environ.get(
